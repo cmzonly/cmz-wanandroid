39a003feda21c4b86e5ecc7cbf8394839fdc7238
mz <cmz456123@163.com>
2024-03-14 11:41:35 +0800

    新增搜索入口

--- module_search/src/main/java/com/example/module_search/viewmodel/SearchViewModel.kt
+++ module_search/src/main/java/com/example/module_search/viewmodel/SearchViewModel.kt
@@ -4,19 +4,13 @@ import com.example.common_lib.base.BaseLiveData
 import com.example.common_lib.base.BaseViewModel
 import com.example.module_search.bean.SearchBean
 import com.example.module_search.repo.SearchRepo
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 
 class SearchViewModel(val searchRepo: SearchRepo) : BaseViewModel() {
     val searchLiveData = BaseLiveData<SearchBean>()
 
     fun search(k : String){
         launch {
-            flow<SearchBean> {
-
-                searchLiveData.value = searchRepo.searchFlow(k)
-            }
+            searchRepo.search(k , searchLiveData)
         }
     }
 }
